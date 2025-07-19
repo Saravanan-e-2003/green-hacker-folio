e8a3ed74513230f14c53ba8b109599d0e27e72f4
Saravanan <94352332+Saravanan-e-2003@users.noreply.github.com>
2025-07-19 07:06:04 +0000

    gh

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: '/green-hacker-folio/',
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    // mode === 'development' &&
+    // componentTagger(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
